63cf526202ff22a9a846fcde067480eb12b5e914
l-ohman <jakelohman7@gmail.com>
2022-09-05 10:19:53 -0500

    CSS details and cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Footer = () => {
     return(
-        <div id="footer">footer</div>
+        <div id="footer">
+            <p>Junior Phase Final Project</p>
+            <p><a href="https://github.com/l-ohman/2208-jpfp">Github</a></p>
+        </div>
     )
 }
 
